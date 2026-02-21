ee6dd930528ab6a14822029b2215897fc0226e4f
Roman Zuchowski <roman.zuchowski@gmail.com>
2026-02-21 15:15:09 +0100

    chore: tidy up tsconfig & linting

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ export default [
     {
         languageOptions: {
             parserOptions: {
-                project: ["./tsconfig.json"],
+                project: ["./tsconfig.json", "./tsconfig.test.json"],
                 tsconfigRootDir: import.meta.dirname,
             },
         },
